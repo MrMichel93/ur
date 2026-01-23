0b39f5d09244022a90317b454b9c973c33c33d9b
Mr. Michel <michel.honore@brewstermadrid.com>
2026-01-23 09:51:20 +0100

    Revert "Add visual primitive components for Carved Wood aesthetic"

--- components/ui/primitives.ts
+++
@@ -1,5 +0,0 @@
-// Export all visual primitive components for easy importing
-export { WoodTextureView } from './WoodTextureView';
-export { TileBase } from './TileBase';
-export { ZigZagBorder } from './ZigZagBorder';
-export { RosetteInlay } from './RosetteInlay';
